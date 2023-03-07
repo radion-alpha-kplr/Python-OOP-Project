f67de83ffcc58f118cb71ebe3eec1a0741b400f8
radion-alpha-kplr <124886260+radion-alpha-kplr@users.noreply.github.com>
2023-03-07 09:30:58 +0000

    juste comments

---
+++ exercices/03.class_tree/exoTree.py
@@ -0,0 +1,8 @@
+# Import des modules nécessaires
+import json
+from unidecode import unidecode
+from treelib import Tree
+import os
+
+# Fonction pour charger les données JSON depuis un fichier et les convertir en dictionnaire Python
+#la fonction json_dict_from_file() reste inchangée.
